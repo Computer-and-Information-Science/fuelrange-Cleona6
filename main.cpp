1f8c83fb939aaa3a49d37b9d9486d2e5f07c8406
CLeonard <cdytlnrd@gmail.com>
2025-09-11 02:04:42 +0000

    added percent full and readjusted equation to get correct values

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@ int main() {
   // Variables
   double tankSize;   // the size of the fuel tank in gallons
   double mpg;        // miles per gallon the car gets
+  int percentFull;   // percentage of tank filled
   double range;      // total distance the car can travel
+
 
   // Prompt user for input
   cout << "Enter the fuel tank size (in gallons): ";
@@ -17,12 +19,15 @@ int main() {
 
   cout << "Enter the miles per gallon (MPG): ";
   cin >> mpg;
+  
+  cout << "Enter the percent the tank is full: ";
+  cin >> percentFull;
 
   // Calculate range
-  range = tankSize * mpg;
+  range = (percentFull * tankSize)/100 * mpg;
 
   // Output result
-  cout << "The car can travel approximately " << range << " miles on a full tank." << endl;
+  cout << "The car can travel approximately " << range << " miles at its current percentage of fuel" << endl;
 
   return 0;
 }
